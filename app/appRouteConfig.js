1a8488603f2bc4c61ad34ad22ca8ed05997b2ed4
Muralimgwl <murali.m@goodworklabs.com>
2016-12-05 10:30:07 +0530

    Revert "added franchies menu,html and controller"
    This reverts commit 03645c5c7ae6f55d8020337686caa09eb57b19e3.

--- app/appRouteConfig.js
+++ app/appRouteConfig.js
@@ -83,22 +83,12 @@ angular.module('app').config(['$logProvider', '$stateProvider', '$urlRouterProvi
         }).
         state('user.franchiesies', {
             url: '/franchiesies',
-            templateUrl: 'app/view/franchiesies/franchiesies.html',
+            templateUrl: 'app/view/franchiesies.html',
             controller: 'franchiesies',
             resolve: {
                 title: function () {
                     return "Franchiesies";
-                },
-
-                posts: function (apiResource, currentUser) {
-                    if (currentUser.getProfile().token) {
-                        return apiResource.getAllPostRelToEmp().$promise;
-                    }
-                    return [];
-
                 }
-
-
 
             }
 
